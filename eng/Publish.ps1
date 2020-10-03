ff686200121cd34a56d77d7681c4d92e47087f78
Vatsan Madhavan <vatsan.madhavan@gmail.com>
2020-10-03 00:20:04 -0700

    Merge master -> release

--- eng/Publish.ps1
+++ eng/Publish.ps1
@@ -1,7 +1,17 @@
+[CmdletBinding(PositionalBinding=$false)]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ArtifactsDirectory, 
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $PrimaryArtifactsSourceAlias
+)
 Install-Module Az.Accounts -Force -Scope AllUsers
 Install-Module Az.Resources -Force -Scop AllUsers
 
-Publish-Module                                                                                                  `
-    -Path "$(System.ArtifactsDirectory)\$(Release.PrimaryArtifactSourceAlias)\Invoke-VsBuild\Invoke-VsBuild\"   `
+Publish-Module                                                                                  `
+    -Path "$(ArtifactsDirectory)\$(PrimaryArtifactsSourceAlias)\Invoke-VsBuild\Invoke-VsBuild\" `
     -NuGetApiKey $(PowerShellGalleryApiKey)
 
